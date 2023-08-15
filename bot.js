ddf2a044a1995e1f3911f74cdf74a88fe3220022
Leow Rezende <leonardo@leowrezende.com.br>
2023-08-15 17:26:06 -0300

    Modificado

--- bot.js
+++ bot.js
@@ -3,15 +3,13 @@ config();
 import { Client, GatewayIntentBits } from 'discord.js';
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.on('ready', () => {
-    console.log('Logged in as ' + client.user.tag);
+    console.log('O cot está pronto pra uso');
 });
 
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
+client.on('message', async msg => {
+    if(msg.content === 'ping'){
+        msg.reply("Pong!");
+    }
 });
 client.login(process.env.BOT_TOKEN);
