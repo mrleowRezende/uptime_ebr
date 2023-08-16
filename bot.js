1f5992bc576b32cd654413e3c4050c002cad84fe
Leow Rezende <leonardo@leowrezende.com.br>
2023-08-16 09:05:55 -0300

    alterado

--- bot.js
+++ bot.js
@@ -7,9 +7,11 @@ client.on('ready', () => {
 });
 
 
-client.on('message', async msg => {
-    if(msg.content === 'ping'){
-        msg.reply("Pong!");
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'ping') {
+      await interaction.reply('Pong!');
     }
 });
 client.login(process.env.BOT_TOKEN);
