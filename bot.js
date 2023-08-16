3e39eb6261a0b484b19fe2b60df7359fa59862ba
Leow Rezende <67129644+mrleowRezende@users.noreply.github.com>
2023-08-16 09:10:39 -0300

    testes

--- bot.js
+++ bot.js
@@ -3,14 +3,9 @@ config();
 import { Client } from 'discord.js';
 const client = new Client({ intents: ['Guilds','GuildMessages'] });
 client.on('ready', () => {
-    console.log('O cot está pronto pra uso');
+    console.log('O bot está pronto pra uso');
 });
 
 
-client.on('message', async msg => {
-    if(msg.content === 'ping'){
-        msg.reply("Pong!");
-    }
-});
 client.login(process.env.BOT_TOKEN);
 
