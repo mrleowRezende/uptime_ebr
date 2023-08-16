1b90067bc4bc86e698f51259124a97e1f44c87eb
Leow Rezende <67129644+mrleowRezende@users.noreply.github.com>
2023-08-16 09:01:12 -0300

    alteracao para teste

--- bot.js
+++ bot.js
@@ -1,16 +1,11 @@
 import { config } from 'dotenv';
 config();
-import { Client, GatewayIntentBits } from 'discord.js';
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+import { Client } from 'discord.js';
+const client = new Client({ intents: ['Guilds','GuildMessages'] });
 client.on('ready', () => {
     console.log('O cot está pronto pra uso');
 });
 
 
-client.on('message', async msg => {
-    if(msg.content === 'ping'){
-        msg.reply("Pong!");
-    }
-});
 client.login(process.env.BOT_TOKEN);
 
