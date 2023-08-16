99753e7123472ee581ce16339d2168f5fc9bed48
Leow Rezende <leonardo@leowrezende.com.br>
2023-08-16 12:03:16 -0300

    bot funcionando

--- bot.js
+++ bot.js
@@ -1,20 +1,36 @@
 import { config } from 'dotenv';
 config();
-import { Client, GatewayIntentBits } from 'discord.js';
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+import { REST, Routes, Client, GatewayIntentBits } from 'discord.js';
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('Bot pronto para uso');
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
+client.on('messageCreate', (message) => {
+    if(message.content === 'ping'){
+        message.react('♥');
+    }
 });
-
 
 client.login(process.env.BOT_TOKEN);
-
